b96c68094344024d92ca8024fcffd335af58f3e0
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-13 10:29:40 +0300

    log function bug update

--- Setup/Database.py
+++ Setup/Database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .Config import APP_Settings
-import log_functions as Terminal_LOG
+from .log_functions import LOG_Database_Connect, LOG_Database_DisConnect
 
 # Define Database Connection
 SQLALCHEMY_DATABASE_URL = f'postgresql://{APP_Settings.POSTOFFICE_DB_USERNAME}:{APP_Settings.POSTOFFICE_DB_PASSWORD}@{APP_Settings.POSTOFFICE_DB_HOSTNAME}:{APP_Settings.POSTOFFICE_DB_PORT}/{APP_Settings.POSTOFFICE_DB_NAME}'
@@ -20,8 +20,8 @@ Base = declarative_base()
 def Create_Database():
 	db = SessionLocal()
 	try:
-		Terminal_LOG.LOG_Database_Connect()
+		LOG_Database_Connect()
 		yield db
 	finally:
-		Terminal_LOG.LOG_Database_DisConnect
+		LOG_Database_DisConnect
 		db.close()
