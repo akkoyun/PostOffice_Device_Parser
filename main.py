c8ddb61e9b978217e28b6a8fba0d4319c8914ea7
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-13 20:24:52 +0300

    code review

--- main.py
+++ main.py
@@ -35,47 +35,48 @@ def Device_Parser():
 			LOG.Line()
 
 			# Declare Variables
-			Module_ID = 0
+			class DB_Variables:
+				Module_ID = 0		# Module ID 
 
-			# Parse Device
-			if Kafka_Message.Info != None:
 
-				# Define DB
-				DB_Device = Database.SessionLocal()
 
-				# Database Query
-				IoT_Module_Query = DB_Device.query(Models.Module).filter(Models.Module.Device_ID.like(Device_ID)).first()
 
-				# Handle Record
-				if not IoT_Module_Query:
 
-					# Create Add Record Command
-					New_Module = Models.Module(Device_ID = Device_ID)
 
-					# Add and Refresh DataBase
-					DB_Device.add(New_Module)
-					DB_Device.commit()
-					DB_Device.refresh(New_Module)
 
-					# Set Module ID
-					Module_ID = New_Module.Module_ID
 
-					# Log
-					RecordedMessage = "New module detected, recording... [" + str(New_Module.Module_ID) + "]"
-					LOG.Service_Logger.debug(RecordedMessage)
 
-				else:
 
-					# Set Module ID
-					
 
-					# LOG
-					LOG.Service_Logger.warning("Module allready recorded, bypassing...")
+
+			# Define DB
+			DB_Device = Database.SessionLocal()
+
+			# Database Query
+			IoT_Module_Query = DB_Device.query(Models.Module).filter(Models.Module.Device_ID.like(Device_ID)).first()
+
+			# Handle Record
+			if not IoT_Module_Query:
+
+				# Create Add Record Command
+				New_Module = Models.Module(Device_ID = Device_ID)
+
+				# Add and Refresh DataBase
+				DB_Device.add(New_Module)
+				DB_Device.commit()
+				DB_Device.refresh(New_Module)
+
+				# Close Database
+				DB_Device.close()
+
+				# Log
+				RecordedMessage = "New module detected, recording... [" + str(New_Module.Module_ID) + "]"
+				LOG.Service_Logger.debug(RecordedMessage)
 
 			else:
 
 				# LOG
-				LOG.Service_Logger.warning("There is no info, bypassing...")
+				LOG.Service_Logger.warning("Module allready recorded, bypassing...")
 
 
 
@@ -88,212 +89,22 @@ def Device_Parser():
 
 
 
-			# Parse Version
-			if Kafka_Message.Info.Firmware != None and Kafka_Message.Info.Hardware != None:
 
-				# Define DB
-				db = Database.SessionLocal()
 
-				# Database Query
-				Version_Query = db.query(Models.Version).filter(
-					Models.Version.Device_ID.like(Device_ID),
-					Models.Version.Firmware_Version.like(Kafka_Message.Info.Firmware),
-					Models.Version.Hardware_Version.like(Kafka_Message.Info.Hardware)).first()
 
-				# Handle Record
-				if Version_Query == None:
 
-					# Create Add Record Command
-					New_Version = Models.Version(
-						Device_ID = Device_ID, 
-						Hardware_Version = Kafka_Message.Info.Hardware,
-						Firmware_Version = Kafka_Message.Info.Firmware)
 
-					# Add and Refresh DataBase
-					db.add(New_Version)
-					db.commit()
-					db.refresh(New_Version)
 
-					# Log 
-					RecordedMessage = "Detected new version, recording... [" + str(New_Version.Version_ID) + "]"
-					LOG.Service_Logger.debug(RecordedMessage)
 
-				else:
-					LOG.Service_Logger.warning("Version allready recorded, bypassing...")
-			else:
-				LOG.Service_Logger.warning("There is no version info, bypassing...")
 
-			# Parse IMU Data
-			if Kafka_Message.Info.Temperature is not None and Kafka_Message.Info.Humidity is not None:
 
-				# Define DB
-				db = Database.SessionLocal()
 
-				# Create Add Record Command
-				New_IMU = Models.IMU(
-					Device_ID = Device_ID,
-					Temperature = Kafka_Message.Info.Temperature,
-					Humidity = Kafka_Message.Info.Humidity)
 
-				# Add and Refresh DataBase
-				db.add(New_IMU)
-				db.commit()
-				db.refresh(New_IMU)
 
-				# Log
-				RecordedMessage = "Detected new IMU data, recording... [" + str(New_IMU.IMU_ID) + "]"
-				LOG.Service_Logger.debug(RecordedMessage)
-			else:
-				LOG.Service_Logger.warning("There is no IMU data, bypassing...")
 
-			# Parse IoT Module
-			if Kafka_Message.IoT.GSM.Module is not None:
 
-				# Define DB
-				db = Database.SessionLocal()
 
-				# Database Query
-				IoT_Module_Query = db.query(Models.IoT_Module).filter(
-					Models.IoT_Module.Module_Firmware.like(Kafka_Message.IoT.GSM.Module.Firmware),
-					Models.IoT_Module.Module_IMEI.like(Kafka_Message.IoT.GSM.Module.IMEI),
-					Models.IoT_Module.Module_Serial.like(Kafka_Message.IoT.GSM.Module.Serial),
-					Models.IoT_Module.Manufacturer_ID == Kafka_Message.IoT.GSM.Module.Manufacturer,
-					Models.IoT_Module.Model_ID == Kafka_Message.IoT.GSM.Module.Model).first()
 
-				# Handle Record
-				if IoT_Module_Query == None:
-
-					# Create Add Record Command
-					New_IoT_Module = Models.IoT_Module(
-						Module_Type = 1,
-						Module_Firmware = Kafka_Message.IoT.GSM.Module.Firmware,
-						Module_IMEI = Kafka_Message.IoT.GSM.Module.IMEI,
-						Module_Serial = Kafka_Message.IoT.GSM.Module.Serial,
-						Manufacturer_ID = Kafka_Message.IoT.GSM.Module.Manufacturer,
-						Model_ID = Kafka_Message.IoT.GSM.Module.Model)
-
-					# Add and Refresh DataBase
-					db.add(New_IoT_Module)
-					db.commit()
-					db.refresh(New_IoT_Module)
-
-					# Log
-					RecordedMessage = "Detected new IoT module, recording... [" + str(New_IoT_Module.Module_ID) + "]"
-					LOG.Service_Logger.debug(RecordedMessage)
-				else:
-					LOG.Service_Logger.warning("IoT module allready recorded, bypassing...")
-			else:
-				LOG.Service_Logger.warning("There is no IoT module data, bypassing...")
-
-			# Parse IoT Location
-			if Kafka_Message.IoT.GSM.Operator.LAC is not None and Kafka_Message.IoT.GSM.Operator.Cell_ID is not None:
-
-				# Define DB
-				db = Database.SessionLocal()
-
-				# Create Add Record Command
-				New_IoT_Location_Post = Models.Location(
-					Device_ID = Device_ID,
-					LAC = Kafka_Message.IoT.GSM.Operator.LAC,
-					Cell_ID = Kafka_Message.IoT.GSM.Operator.Cell_ID)
-
-				# Add and Refresh DataBase
-				db.add(New_IoT_Location_Post)
-				db.commit()
-				db.refresh(New_IoT_Location_Post)
-
-				# Log
-				RecordedMessage = "Detected new location, recording... [" + str(New_IoT_Location_Post.Location_ID) + "]"
-				LOG.Service_Logger.debug(RecordedMessage)
-			else:
-				LOG.Service_Logger.warning("There is no location data, bypassing...")
-
-			# Parse IoT SIM
-			if Kafka_Message.IoT.GSM.Operator.ICCID is not None:
-
-				# Define DB
-				db = Database.SessionLocal()
-
-				# Database Query
-				IoT_SIM_Query = db.query(Models.SIM).filter(
-					Models.SIM.ICCID.like(Kafka_Message.IoT.GSM.Operator.ICCID),
-					Models.SIM.Operator_ID == Kafka_Message.IoT.GSM.Operator.Code).first()
-
-				# Handle Record
-				if IoT_SIM_Query is None:
-
-					# Create Add Record Command
-					New_IoT_SIM_Post = Models.SIM(
-						Device_ID = Device_ID,
-						ICCID = Kafka_Message.IoT.GSM.Operator.ICCID,
-						Operator_ID = Kafka_Message.IoT.GSM.Operator.Code)
-
-					# Add and Refresh DataBase
-					db.add(New_IoT_SIM_Post)
-					db.commit()
-					db.refresh(New_IoT_SIM_Post)
-
-					# Log
-					RecordedMessage = "Detected new SIM, recording... [" + str(New_IoT_SIM_Post.SIM_ID) + "]"
-					LOG.Service_Logger.debug(RecordedMessage)
-				else:
-					LOG.Service_Logger.warning("SIM allready recorded, bypassing...")
-			else:
-				LOG.Service_Logger.warning("There is no SIM data, bypassing...")
-
-			# Parse IoT Connection
-			if Kafka_Message.IoT.GSM.Operator.RSSI is not None:
-
-				# Define DB
-				db = Database.SessionLocal()
-
-					# Database Query
-				IoT_Existing_SIM_Query = db.query(Models.SIM).filter(
-					Models.SIM.Device_ID.like(Device_ID)).first()
-
-				# Refresh DataBase
-				db.refresh(IoT_Existing_SIM_Query)
-
-				# Handle SIM
-				if IoT_Existing_SIM_Query is not None:
-
-					# Create Add Record Command
-					New_IoT_Connection_Post = Models.Connection(
-						Device_ID = Device_ID,
-						SIM_ID = IoT_Existing_SIM_Query.SIM_ID,
-						RSSI = Kafka_Message.IoT.GSM.Operator.RSSI,
-						Device_IP = Device_IP,
-						Connection_Time = Kafka_Message.IoT.GSM.Operator.ConnTime,
-						Data_Size = len(Message.value))
-
-					# Add and Refresh DataBase
-					db.add(New_IoT_Connection_Post)
-					db.commit()
-					db.refresh(New_IoT_Connection_Post)
-
-					# Lof
-					RecordedMessage = "Detected new connection data, recording... [" + str(New_IoT_Connection_Post.Connection_ID) + "]"
-					LOG.Service_Logger.debug(RecordedMessage)
-				else:
-					LOG.Service_Logger.error("SIM Data Error...")
-			else:
-				LOG.Service_Logger.warning("There is no connection data, bypassing...")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			# Close Database
-			db.close()
 
 			# Commit Message
 			Kafka_Consumer.commit()
