5e91ecfd1e13b77b7a58493ec0499cdddf918e59
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-14 00:58:20 +0300

    code review

--- main.py
+++ main.py
@@ -68,8 +68,7 @@ def Device_Parser():
 			else:
 
 				# Set Variable
-#				Variables.Module_ID = np.array(list(Query_Module.__dict__.items()))[5,1]
-				Variables.Module_ID = Schema.Module(Query_Module.__dict__.items())
+				Variables.Module_ID = np.array(list(Query_Module.__dict__.items()))[5,1]
 
 				# LOG
 				LOG.Service_Logger.warning(f"Module allready recorded [{Variables.Module_ID}], bypassing...")
