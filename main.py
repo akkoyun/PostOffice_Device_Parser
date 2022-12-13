3167078bbc948b40c8cc87b8a4bd2bd004c629bc
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-13 21:16:55 +0300

    code review

--- main.py
+++ main.py
@@ -61,6 +61,9 @@ def Device_Parser():
 			Query_Module = DB_Module.query(Models.Module).filter(Models.Module.Device_ID.like(Device_ID)).first()
 
 			print(type(Query_Module))
+			M = json.dumps(Query_Module.__dict__)
+			print(M)
+			print(type(M))
 
 			# Handle Record
 			if not Query_Module:
